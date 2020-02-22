5af7f664c8f551016014541734849fda4635ff43
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-02-22 14:23:31 -0600

    add additional routes

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const Sequelize = require('sequelize');
+const bcryptjs = require('bcryptjs');
 
 const db = require('./db');
 const { Course, User } = db.sequelize.models;
@@ -33,6 +34,7 @@ router.get('/users', asyncHandler(async (req, res, next) => {
 // POST user(s)
 router.post('/users', asyncHandler(async (req, res, next) => {
   try {
+    req.body.password = await bcryptjs.hashSync(req.body.password);
     const user = await User.create(req.body);
     res.status(201).end();
   } catch (error) {
@@ -49,7 +51,14 @@ router.post('/users', asyncHandler(async (req, res, next) => {
 // GET course(s)
 router.get('/courses', asyncHandler(async (req, res, next) => {
   try {
-    const courses = await Course.findAll();
+    const courses = await Course.findAll({
+      include: [
+        {
+          model: User,
+          as: 'owner'
+        }
+      ]
+    });
     res.status(200).json(courses);
   } catch (error) {
     throw error;
@@ -58,32 +67,61 @@ router.get('/courses', asyncHandler(async (req, res, next) => {
 
 // GET course(s) by Id
 router.get('/courses/:id', asyncHandler(async (req, res, next) => {
-  // if (!user) {
-    
-  // } else {
-  //   res.status(200).json({user});
-  // }
+  let course;
+  try {
+    course = await Course.findByPk(req.params.id);
+    if (course === null) {
+      throw new Error('Course not found');
+    } else {
+      res.status(200).json(course);
+    }
+  } catch (error) {
+    if (course === null) {
+      console.log(error);
+      res.status(404).json({message: error.message});
+    } else {
+      throw error;
+    }
+  }
 }));
 
 // POST course(s)
 router.post('/courses', asyncHandler(async (req, res, next) => {
-  // const user = req.body;
-  // res.status(201);
-  // users.push(user);
+  try {
+    // req.body.userId = await User.find
+    const course = await Course.create(req.body);
+    res.status(201).end();
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const errorMessages = [];
+      error.errors.map(error => errorMessages.push(error.message));
+      res.status(400).json({message: errorMessages});
+    } else {
+      throw error;
+    }
+  }
 }));
 
 // PUT (update) course(s)
-router.put('/courses', asyncHandler(async (req, res, next) => {
-  // const user = req.body;
-  // res.status(201);
-  // users.push(user);
+router.put('/courses/:id', asyncHandler(async (req, res, next) => {
+  try {
+    const course = await Course.findByPk(req.params.id);
+    await course.update(req.body);
+    res.status(204).end();
+  } catch (error) {
+    throw error;
+  }
 }));
 
 // DELETE course(s)
 router.delete('/courses', asyncHandler(async (req, res, next) => {
-  // const user = req.body;
-  // res.status(201);
-  // users.push(user);
+  try {
+    const course = await Course.findByPk(req.params.id);
+    await course.destroy();
+    res.status(204).end();
+  } catch (error) {
+    throw error;
+  }
 }));
 
 module.exports = router;
